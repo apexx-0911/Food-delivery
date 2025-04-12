41e3b271591292e82ab5f055e81dcddb52a1283c
apexx-0911 <apekshasharma091103@gmail.com>
2025-04-13 00:09:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div",{id : "parent"}, 
-    React.createElement("div", {id:"child"}, 
-    [React.createElement("h1", {id:"h1"}, "Smile krooo"), React.createElement("h2", {id:"h2"}, "Smile krooooo")]));
-    const body = document.querySelector("body");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+const headComponent = () => {
+    return (<div><h1 className="head"> hello from jsx headComponent</h1></div>)
+};
+const heading = (<div><h1 className="head"> hello from jsx</h1></div>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headComponent());
